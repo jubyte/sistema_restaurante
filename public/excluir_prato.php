10e8e31540ddfe02c89dabb116056ef66f09d3b1
Julia <julia_ferreira3@estudante.sesisenai.org.br>
2026-08-19 08:14:41 -0300

    feat - função de excluir dos pratos

---
+++ public/excluir_prato.php
@@ -0,0 +1,14 @@
+<?php
+include "../infra/conexao.php";
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM pratos WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+header("Location: ../index.php");
+exit();
+?> 
