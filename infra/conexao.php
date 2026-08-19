6a3b84585007e94acd011ef237d9a42ad4414996
Julia <julia_ferreira3@estudante.sesisenai.org.br>
2026-08-19 07:40:12 -0300

    add - conexão feita

---
+++ infra/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "restaurante";
+
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conexao->connect_error) {
+    die("Erro na conexão: " . $conexao->connect_error);
+};
+?>
